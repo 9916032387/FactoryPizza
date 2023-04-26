f938c770e065bba604679b04887af087bc5a7b31
Sameer <131773168+RajekhanavarSameer@users.noreply.github.com>
2023-04-26 12:13:37 +0530

    Add files via upload

---
+++ scripts.groovy
@@ -0,0 +1,50 @@
+
+job('job-dsl-checkout') {
+    
+    scm {
+        github('tetradev01/hello-world-war', 'master')
+    }
+      
+   publishers {
+        downstream 'job-dsl-compile', 'SUCCESS'
+    }
+    
+}
+
+mavenJob('job-dsl-compile'){
+   
+  customWorkspace('/var/lib/jenkins/workspace/job-dsl-checkout')
+  mavenInstallation('Maven 3.3.9')
+  goals('compile')
+    
+  publishers {
+        downstream 'job-dsl-package', 'SUCCESS'
+   }
+}
+
+mavenJob('job-dsl-package'){
+    customWorkspace('/var/lib/jenkins/workspace/job-dsl-checkout')
+    mavenInstallation('Maven 3.3.9')
+    goals('package')
+    
+  publishers {
+        downstream 'job-dsl-deploy', 'SUCCESS'
+  }
+}
+
+job('job-dsl-deploy') {
+    description 'Deploy app to the demo server'
+    
+    steps{
+             shell 'sshpass -p "123456" scp /var/lib/jenkins/workspace/job-dsl-checkout/target/hello-world-war-1.0.0.war release@10.12.108.11:/opt/tomcat/webapps/'
+      }
+}
+
+ deliveryPipelineView('job-dsl delivery pipeline') {
+    showAggregatedPipeline true
+    enableManualTriggers true
+    pipelineInstances 5
+    pipelines {
+        component('job-dsl delivery pipeline', 'job-dsl-checkout')
+    }
+ }
